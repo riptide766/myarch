dbd3d412dff76ec8afa72541d69a6a05b63673e3
mattmonkey <mattmonkey@sina.com>
2012-08-02 16:27:22 +0800

    完善窗口控制脚本

--- shortkey.d/jump_or_exec.sh
+++ shortkey.d/jump_or_exec.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 # 切换或打开一个程序窗口 
-#  
+# 切换同一种程序的不同窗口
+#
 # Date: 20120801
 # Author: riptide
 
+# 配置区开始
 #
 # 映射格式 [shortname]="classname|command"
 # classname可以通过wmctrl -lx查看
@@ -20,9 +22,14 @@ declare -A mapping=(
 	[thunar]="Thunar.Thunar|thunar"
 )
 
+switchkey="Alt + grave"
+	
 tagstr="# added by jump_or_exec"
 
 config_file=$(readlink -m "/home/`whoami`/.xbindkeysrc")
+
+# 配置区结束
+
 
 clear_oldsetting(){
 	grep -q "$tagstr" $config_file || return
@@ -45,6 +52,7 @@ setting(){
 	for key in $keys; do
 		out+="\"$cmd -e $key\"\n\tMod4 + $(( `get_pos $key` - $base ))\n\n"
 	done
+	out+="\"$cmd -S\"\n\t$switchkey\n\n"
 	clear_oldsetting
 	echo -e $tagstr"\n"$out$tagstr >> $config_file
 	killall -HUP xbindkeys
@@ -55,11 +63,27 @@ execution(){
 	wmctrl -a ${entry%\|*} -x || exec ${entry#*\|} &
 }
 
+switch_same_app(){
+	data=$(wmctrl -lxp | grep `echo $(xprop -root | grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}' | sed 's/,//' | sed 's/^0x/0x0/')`)
+	ccls=$(echo $data | cut -d" " -f 4)
+	cwid=$(echo $data | cut -d" " -f 1)
+	cwks=$(echo $data | cut -d" " -f 2)
+	list=$(wmctrl -lxp | awk -v wks=$cwks -v cls=$ccls '{ if ( $2 == wks && $4 == cls ) print $1 }')
+	list=$list" "$list
+	twid=$(echo ${list#*$cwid} | cut -d" " -f 1)
+	wmctrl -i -a $twid
+}
+
 usage(){
 cat <<EOF
+
 -s      添加快捷键配置到.xbindkeysrc
--e app  切换或打开程序窗口	
+-e app  切换或打开一个程序窗口	
+-S      切换到同一种程序的其他窗口
 -h      帮助信息
+
+配置方法见脚本。
+
 EOF
 }
 
@@ -68,11 +92,14 @@ if [[ -z $1 || $1 = @(-h|--help) ]]; then
   exit $(( $# ? 0 : 1 ))
 fi
 
-while getopts she: opt ; do
+while getopts sShe: opt ; do
 	case $opt in
 		s) 
 			setting
 			echo done...
+			;;
+		S) 
+			switch_same_app
 			;;
 		e) 
 			execution $OPTARG
