4c605f94c94682bc9e5780a7c4cc1db965f71613
riptide766 <riptide766@gmail.com>
2012-12-19 21:50:01 +0800

    backup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -110,8 +110,6 @@ mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesom
                                   },
                         })
 
---mymainmenu.menu_keys.up={"j","k"}
-
 mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
                                      menu = mymainmenu })
 -- }}}
@@ -129,16 +127,55 @@ cpuwidget = widget({ type = "textbox" })
 vicious.register(cpuwidget, vicious.widgets.cpu, "$1%")
 -- }}}
 
--- Initilize widget
-volwidget = widget({ type = "textbox" })
--- Register widget
-vicious.register(volwidget, vicious.widgets.volume, " $1% ", 2, "Master")
+-- {{{2 Volume Control
+volume_cardid  = 0
+volume_channel = "Master"
+function volume (mode, widget)
+  if mode == "update" then
+    local fd = io.popen("amixer -c " .. volume_cardid .. " -- sget " .. volume_channel)
+    local status = fd:read("*all")
+    fd:close()
+
+    local volume = string.match(status, "(%d?%d?%d)%%")
+    volume = string.format("% 3d", volume)
+
+    status = string.match(status, "%[(o[^%]]*)%]")
+
+    if string.find(status, "on", 1, true) then
+      volume = '𝅘𝅥𝅮' .. volume .. "%"
+    else
+      volume = '𝅘𝅥𝅮' .. volume .. '<span color="red">M</span>'
+    end
+    widget.text = volume
+  elseif mode == "up" then
+    io.popen("amixer -q -c " .. volume_cardid .. " sset " .. volume_channel .. " 5%+"):read("*all")
+    volume("update", widget)
+  elseif mode == "down" then
+    io.popen("amixer -q -c " .. volume_cardid .. " sset " .. volume_channel .. " 5%-"):read("*all")
+    volume("update", widget)
+  else
+    io.popen("amixer -c " .. volume_cardid .. " sset " .. volume_channel .. " toggle"):read("*all")
+    volume("update", widget)
+  end
+end
+volume_clock = timer({ timeout = 10 })
+volume_clock:add_signal("timeout", function () volume("update", tb_volume) end)
+volume_clock:start()
+
+tb_volume = widget({ type = "textbox", name = "tb_volume", align = "right" })
+tb_volume.width = 35
+tb_volume:buttons(awful.util.table.join(
+  awful.button({ }, 4, function () volume("up", tb_volume) end),
+  awful.button({ }, 5, function () volume("down", tb_volume) end)
+  --awful.button({ }, 1, function () volume("mute", tb_volume) end)
+))
+-- }}}
 
 -- {{{ Memory
 mymemicon = widget({ type = 'imagebox', name = 'mymemicon'})
 mymemicon.image = image(beautiful.widget_mem)
 memwidget = widget({ type = 'textbox', name = 'memwidget'})
-vicious.register(memwidget, vicious.widgets.mem, '$2M')
+vicious.register(memwidget, vicious.widgets.mem, '$2M-$6M')
 -- }}}
 
 -- {{{ Battery
@@ -233,9 +270,9 @@ for s = 1, screen.count() do
         mylayoutbox[s],
 		s == 1 and mysystray or nil,
         mytextclock,
+		myseparator, tb_volume , aymemicon ,
 		myseparator, memwidget , aymemicon ,
 		myseparator, cpuwidget , aymemicon ,
-		myseparator, volwidget , aymemicon ,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
     }
@@ -461,7 +498,7 @@ autostart.run_once_list{
 	"firefox",
 	"amule" ,
 	"thunar" ,
-	{"terminator" ,"terminator -T desktop -e ' exec tmux' " },
+	{"terminator" ,"terminator -T desktop -e ' exec tmux attach -t work' " },
 }
 
 awful.menu.menu_keys.up = { "k" }
